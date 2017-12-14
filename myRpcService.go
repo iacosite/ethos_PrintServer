3a68f25a59fb543bfd94501110c06f931c64e833
gabrielemilauro <milaurog@gmail.com>
2017-12-13 21:30:21 -0600

    v1

--- myRpcService.go
+++ myRpcService.go
@@ -12,8 +12,11 @@ var myRpc_increment_counter uint64 = 0
 var myRpc_count uint64 = 0
 var logger = log.Initialize("test/myRpcService")
 
-//create a buffered channel of 2 elements
-var sem = make(chan uint64, 2)
+//list of files, we set set an initial dimension, but slices in go can be easily resized
+
+
+//create a buffered channel
+var sem = make(chan uint64)
 
 func init() {
 	SetupMyRpcIncrement(increment)
@@ -31,59 +34,9 @@ func increment(quantity uint64) (MyRpcProcedure) {
 
 func boxhandle(buff Box) (MyRpcProcedure) {
 	myRpc_count += 1
-// tried to implement a sleep with Beep, but it doesn't sleep the amount of time we want
-//		var time syscall.Time64
-//		time = 5*1000000000 // 5 seconds(time is in nanoseconds)
-//		logger.Printf("Wait 5 seconds\n")
-//		_ ,status := syscall.Beep(time)//similar to sleep
-//		if status != syscall.StatusOk {
-//			logger.Fatalf ("Error Beep syscall\n")
-//		}
-//		logger.Printf("resume\n")
-
 
 	sem <- 1//it's like a signal(sem+1), it blocks if sem==MAX, that is the buffer(sem) is full
 	logger.Printf("myRpcService: Box received %v and sent to the printer\n", buff)
-
-//Assuming that we have 6 seconds of execution with sudo ethosRun -t
-//It blocks at the third block if the buffer(sem) length is 2 and the printer takes 6 seconds to process 1 Box
-//It doesn't block if the buffer(sem) length is 2 and the printer takes 1 secondto process 1 Box
-
-
-//implement our printer as a concurrent thread with a goroutine
-	go func() {
-
-	//implement a sleep with a loop
-	//the sleep stands for the time the printer takes to process 1 Box
-		time1 := syscall.GetTime()
-
-		logger.Printf("start wait\n")
-		var i uint64 = 1
-	//it waits around 1 sec with 3*10^9, so for instance 6*(3*10^9) it's around 6 sec
-		for i <= 3000000000*6 {
-			i = i+1
-		}
-		time2 := syscall.GetTime()
-		logger.Printf("time waited in ms %v\n", (time2-time1)/1000000)
-
-		<-sem //it's like a wait(sem-1), it blocks if sem==0(in our case it never blocks, it simply means that we have finished to process our Box and so we decrement the buffer(sem) by 1)
-	}()
-
-
-//implement a sleep with a loop
-//	time1 := syscall.GetTime()
-//	logger.Printf("start wait\n")
-//	var i uint64 = 1
-//it waits around 1 sec with 3*10^9, so for instance 6*(3*10^9) it's around 6 sec
-//	for i <= 3000000000*6 {
-//		i = i+1
-//	}
-//	time2 := syscall.GetTime()
-//	logger.Printf("time waited in ms %v\n", (time2-time1)/1000000)
-
-
-//tried to implement a sleep with time.Sleep, but you can't use time.Second or time.Millisecond to put inside like for instance time.Sleep(time.Second * 3)
-
 
 	return &MyRpcBoxReply{myRpc_count}
 }
@@ -110,6 +63,22 @@ func chunk(chunk []byte) (MyRpcProcedure) {
 //}
 
 func main () {
+
+//implement our printer as a concurrent thread with a goroutine
+	go func() {
+		<-sem //it's like a wait(sem-1), it blocks if sem==0(in our case it never blocks, it simply means that we have finished to process our Box and so we decrement the buffer(sem) by 1)
+	//the sleep stands for the time the printer takes to process 1 Box
+		var time syscall.Time64
+
+		time = 6*1000000000 // 1 seconds(time is in nanoseconds)
+
+	//sleep until current time + time
+		altEthos.Beep(altEthos.GetTime()+time)
+
+	}()
+
+
+
 	listeningFd, status := altEthos.Advertise("myRpc")
 	if status != syscall.StatusOk {
 		logger.Printf("Advertising service failes %s\n", status)
--- test.sh
+++ test.sh
@@ -3,4 +3,3 @@ cd server
 sudo ethosRun -t
 ethosLog .
 cd ..
-sudo make clean >> /dev/null
