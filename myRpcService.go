cd6dab2bf6e7f0eda81ea9bb9c6d39a5de0e258c
Iacopo Olivo <iacopo.olivo@gmail.com>
2017-12-13 21:35:18 -0600

    Merged

--- myRpcService.go
+++ myRpcService.go
@@ -36,48 +36,8 @@ func increment(quantity uint64) (MyRpcProcedure) {
 
 func boxhandle(buff Box) (MyRpcProcedure) {
 	myRpc_count += 1
-<<<<<<< HEAD
-// tried to implement a sleep with Beep, but it doesn't sleep the amount of time we want
-
-=======
->>>>>>> 3a68f25a59fb543bfd94501110c06f931c64e833
-
 	sem <- 1//it's like a signal(sem+1), it blocks if sem==MAX, that is the buffer(sem) is full
 	logger.Printf("myRpcService: Box received %v and sent to the printer\n", buff)
-
-<<<<<<< HEAD
-//Assuming that we have 6 seconds of execution with sudo ethosRun -t
-//It blocks at the third block if the buffer(sem) length is 2 and the printer takes 6 seconds to process 1 Box
-//It doesn't block if the buffer(sem) length is 2 and the printer takes 1 secondto process 1 Box
-
-
-//implement our printer as a concurrent thread with a goroutine
-	go func() {
-
-	//implement a sleep with a loop
-	//the sleep stands for the time the printer takes to process 1 Box
-		time1 := syscall.GetTime()
-
-		logger.Printf("start wait\n")
-		var i uint64 = 1
-	//it waits around 1 sec with 3*10^9, so for instance 6*(3*10^9) it's around 6 sec
-		for i <= 3000000000*6 {
-			i = i+1
-		}
-		time2 := syscall.GetTime()
-		logger.Printf("time waited in ms %v\n", (time2-time1)/1000000)
-
-		<-sem //it's like a wait(sem-1), it blocks if sem==0(in our case it never blocks, it simply means that we have finished to process our Box and so we decrement the buffer(sem) by 1)
-	}()
-
-
-
-
-//tried to implement a sleep with time.Sleep, but you can't use time.Second or time.Millisecond to put inside like for instance time.Sleep(time.Second * 3)
-
-
-=======
->>>>>>> 3a68f25a59fb543bfd94501110c06f931c64e833
 	return &MyRpcBoxReply{myRpc_count}
 }
 
